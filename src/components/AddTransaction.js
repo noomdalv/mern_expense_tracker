555034f18074f6653afcfa93a072275d27df6275
noomdalv <vladlunasan@gmail.com>
2023-02-13 16:28:49 -0500

    added delete/add transaction functions

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+
+import { GlobalContext } from '../context/GlobalState';
 
 export default function AddTransaction() {
+    const { addTransaction } = useContext(GlobalContext);
+
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        const newTx = {
+            id: Math.floor(Math.random() * 100000000),
+            text,
+            amount: +amount
+        }
+
+        addTransaction(newTx)
+    }
 
     return (
         <div>
             <h3>Add new transaction</h3>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className="form-control">
                     <label htmlFor="text">Text</label>
                     <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
