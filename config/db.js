af6f2cc8cad0fcae1eeae2ba3f8426e9d0be04e9
noomdalv <vladlunasan@gmail.com>
2023-02-13 22:45:55 -0500

    added mongodb connection

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        mongoose.set("strictQuery", false);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+
+        console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold)
+    } catch (error) {
+        console.log(`Error: ${error.message}`.red);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
