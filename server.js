5a3a0a432ff02cdc12bd8d68df8422388669f723
noomdalv <vladlunasan@gmail.com>
2023-02-14 00:44:01 -0500

    added transaction controller api

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ const colors = require('colors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 
-dotenv.config({ path: './config/config.env' })
+dotenv.config({ path: './config/config.env' });
 
 connectDB();
 
-const transactions = require('./routes/transactions')
+const transactions = require('./routes/transactions');
 
 const app = express();
 
-app.use('/api/v1/transactions', transactions)
+app.use(express.json());
+
+app.use('/api/v1/transactions', transactions);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
